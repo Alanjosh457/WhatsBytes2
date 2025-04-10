d1bca80dcee9295469dfb4724f6bd1b1a30b5d54
Alanjosh457 <alanjoshua515-email@gmail.com>
2025-04-10 22:05:05 +0530

    fix: escaped apostrophe in Login page

--- app/page.js
+++ app/page.js
@@ -11,18 +11,12 @@ export default function Home() {
         <p className="text-lg text-gray-600 mb-8">Connect your Google Sheets seamlessly.</p>
 
         <div className="flex space-x-6 justify-center">
-          <button
-            onClick={() => router.push("/Login")}
-            className="px-6 py-3 bg-blue-600 text-white text-lg rounded-lg shadow-md hover:bg-blue-700 transition"
-          >
+       
             Login
-          </button>
-          <button
-            onClick={() => router.push("/register")}
-            className="px-6 py-3 bg-green-600 text-white text-lg rounded-lg shadow-md hover:bg-green-700 transition"
-          >
+     
+         
             Register
-          </button>
+         
         </div>
       </div>
     </div>
